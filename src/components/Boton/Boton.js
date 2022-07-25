1479bc14aa3192a4b56c923b644655ffab4552ed
Alfredo-Calvelo <alfredocalvelo1@gmail.com>
2022-07-25 16:43:04 -0300

    boton tiendas online

--- src/components/Boton/Boton.js
+++ src/components/Boton/Boton.js
@@ -3,9 +3,25 @@ import { BsChevronDown } from 'react-icons/bs'
 import { useSelector, useDispatch } from 'react-redux'
 import { activarMenuDesplegable } from '../../redux/actions'
 import { useNavigate } from 'react-router-dom'
+import { useEffect, useState } from 'react'
 
 export default function Boton (props){
   const navigate = useNavigate()
+  
+  const Tiendas = useSelector(state=>state.Tiendas)
+  const [tiendasFinales, setTiendasFinales] = useState([])
+  useEffect(()=>{
+    if (Tiendas && Tiendas[0]?.tiendas) {
+      let copiaTiendas =[] 
+      Tiendas[0].tiendas.map((elem, index)=>{
+        copiaTiendas.push({title:elem.title, link:elem.URL, tipo:'producto'})
+      })
+      
+      setTiendasFinales(copiaTiendas)
+    }
+  },[Tiendas])
+
+
   let dropMenu= useSelector(state=>state.menuDesplegable)
   if (!props.dropMenu)dropMenu=false
   const dispatch = useDispatch()
@@ -14,15 +30,16 @@ export default function Boton (props){
       dispatch(activarMenuDesplegable(false))
     }else dispatch(activarMenuDesplegable(true))
   }
-  function hola(params) {
+  function navegar(params) {
     navigate(props?.ruta)
   }
+
   return(
     <a className={styles.containerLink} style={{textDecoration:'none'}} href={props.link?props.link:null} download={props.download?props.download:false} target={props.actual?'':`_blank`}>
     <div className={styles.container}>
     <div style={{border:props.color?`1px solid ${props.color}`:''}} className={props.relleno?`${styles.relleno} ${dropMenu?styles.adelante:''}`:`${styles.noRelleno} ${dropMenu?styles.adelante:''}`} onClick={()=>{
       if(props.dropMenu){dropMenuFunction()}
-      if (props.ruta){hola()}
+      if (props.ruta){navegar()}
       if (props.click) {props.click()} 
     }}>
       <span style={{color:props.color?props.color:''}} className={styles.text}>{props.text}</span>
@@ -38,15 +55,15 @@ export default function Boton (props){
       {props.dropMenu?
         <ul className={`${styles.lista}`}>
           <div className={`${styles.listContainer} ${dropMenu?styles.listaActiva:styles.listaInactiva}`}>
-            <span className={`${styles.text} ${styles.li}`}>ARGENTINA</span>
-            <span className={`${styles.text} ${styles.li}`}>URUGUAY</span>
-            <span className={`${styles.text} ${styles.li}`}>USA</span>
+            {tiendasFinales.map((elem,index)=>{
+              return(
+                <a key={index} href={elem.link} className={`${styles.text} ${styles.li}`}>{elem.title.toUpperCase()}</a>
+              )
+            })}
           </div>
         </ul>
       :null  
     }
-        
-        
 
 
     </div>
